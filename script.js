c542f53a2120ef37ce3ac4996d8d8c22aac489a0
Able Tsang <abletsang@gmail.com>
2017-05-15 22:06:10 -0700

    added selectors for all the buttons

---
+++ script.js
@@ -0,0 +1,20 @@
+var one = document.querySelector(".one");
+var two = document.querySelector(".two ");
+var three = document.querySelector(".three");
+var four = document.querySelector(".four");
+var five = document.querySelector(".five");
+var six = document.querySelector(".six");
+var seven = document.querySelector(".seven");
+var eight = document.querySelector(".eight");
+var nine = document.querySelector(".nine");
+var zero = document.querySelector(".zero");
+
+var clear = document.querySelector(".clear");
+var decimal = document.querySelector(".decimal");
+var divide = document.querySelector(".divide");
+var multiply = document.querySelector(".multiply");
+var plus = document.querySelector(".plus");
+var minus = document.querySelector(".minus");
+var equals = document.querySelector(".equals");
+var output = document.querySelector(".output");
+// var  = document.querySelector(".");
